7aba909c69598b6b4e57d6ce5503f13fe8687056
lukaszuznanski <lukaszuznanski94@gmail.com>
2016-02-16 01:58:15 +0100

    categories on main view

--- src/ShopBundle/Controller/DefaultController.php
+++ src/ShopBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use ShopBundle\Entity\Products;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ShopBundle:Default:index.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery('SELECT DISTINCT a.category FROM ShopBundle:Products a');
+        $categories = $query->getResult();
+        return $this->render('ShopBundle:Default:index.html.twig', array('categories' => $categories));
     }
+   
 }
